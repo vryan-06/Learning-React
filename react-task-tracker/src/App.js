780fe0edaac85ab37e5c1df725737bb3d45c42d0
V Ryan <ryan19vt@gmail.com>
2022-10-02 00:25:21 +0530

    Adding Tasks successfully

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,42 +1,67 @@
 import { useState } from "react";
 import Header from "./components/Header";
-import Tasks from './components/Tasks';
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 function App() {
   const [tasks, setTasks] = useState([
     {
-        id: 1,
-        text: "Chess game",
-        day: "02/10/2022 2pm",
-        reminder: true,
+      id: 1,
+      text: "Chess game",
+      day: "02/10/2022 2pm",
+      reminder: true,
     },
     {
-        id: 2,
-        text: "TCS assignment",
-        day: "04/10/2022 10am",
-        reminder: false,
+      id: 2,
+      text: "TCS assignment",
+      day: "04/10/2022 10am",
+      reminder: false,
     },
     {
-        id: 3,
-        text: "GDSC meet",
-        day: "01/10/2022 9pm",
-        reminder: true,
+      id: 3,
+      text: "GDSC meet",
+      day: "01/10/2022 9pm",
+      reminder: true,
     },
-]);
+  ]);
+  
+  //Creating function to add events after form submission
+  const addTask = (task) => {
+    // console.log(task)
+    const id = Math.floor(Math.random()*10000)+1
 
-// Creating function to delete events when 'X' is clicked
-const deleteTask = (id)=> {
-  // For checking purposes
-  console.log('delete',id) 
+    const newTask ={id, ...task}
+    setTasks([...tasks, newTask])
+  }
 
-  // This will filter i.e not show the task id whose 'X' was clicked
-  setTasks(tasks.filter((task)=>task.id!=id))
-}
+  // Creating function to delete events when 'X' is clicked
+  const deleteTask = (id) => {
+    // For checking purposes
+    console.log("delete", id);
+
+    // This will filter i.e not show the task id whose 'X' was clicked
+    setTasks(tasks.filter((task) => task.id != id));
+  };
+
+  // Function to change reminder class
+  const toggleReminder = (id) => {
+    console.log("Toggled reminder for ID: ", id);
+    setTasks(
+      tasks.map((task) =>
+        task.id == id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
 
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} onDelete={deleteTask} />
+      <AddTask onAdd={addTask}/>
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No tasks present"
+      )}
     </div>
   );
 }
