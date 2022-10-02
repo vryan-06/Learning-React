d2302cce550ae220cbea98f0a6534ec8965fcbe8
V Ryan <ryan19vt@gmail.com>
2022-10-02 15:18:38 +0530

    Created production build

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -4,7 +4,7 @@ import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 
 function App() {
-  const [showAddTask, setShowAddTask] = useState(true);
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     {
       id: 1,
